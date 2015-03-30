03e2ab0a9775e9f90ac44d23a00459f6e7bb1797
vishalsubbiah <vishal.subbiah94@gmail.com>
2015-03-30 12:39:10 +0530

    single click only
    removed double tap and replaced with a single tap

--- js/script.js
+++ js/script.js
@@ -13,3 +13,59 @@ function gotopage(id)
 {
 window.location.href='data/'+id+'.html';
 }
+function gotoroom(id)
+{
+file_name=document.getElementById(id).value;
+location.href=file_name+'.html';
+}
+function gotoroomno()
+{
+	file_name=document.getElementById('textbox').value;
+location.href='ESB'+file_name+'.html';
+}
+function combo_1(id)
+{
+	gotopage(id);
+	voice(id);
+	
+}
+function combo_2(id)
+{
+	gotoroom(id);
+	voice('go');
+	
+}
+
+function Inputall(t)
+{
+document.getElementById("textbox").value=document.getElementById('textbox').value+t;
+}
+function ClearAll()
+{
+	document.getElementById("textbox").value='';
+}
+function ClearOne()
+{
+	l=document.getElementById("textbox").value.length;
+	document.getElementById("textbox").value=document.getElementById("textbox").value.slice(0,l-1);
+}
+
+function combo_3(id)
+{
+	voice(id);
+	Inputall(id);
+}
+function combo_4(id)
+{
+	voice_content(id);
+	gotoroomno();
+}
+function combo_5(id)
+{
+	voice(id);
+	ClearAll();
+	}
+function combo_6(id){
+voice(id);
+ClearOne();
+}
